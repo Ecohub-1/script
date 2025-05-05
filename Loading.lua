f0ce89f0a3d1553fea046a4bc14a2e454110147f
Ecohub-1 <boyyanamalila@gmail.com>
2025-05-05 07:05:45 +0700

    Update Loading.lua

--- Loading.lua
+++ Loading.lua
@@ -1,1 +1,13 @@
+local placeScripts = {
+    [751310835] = "DrillGame.lua",
+}
 
+local placeId = game.PlaceId
+local scriptName = placeScripts[placeId]
+
+if scriptName then
+    local url = ("https://raw.githubusercontent.com/Ecohub-1/Ecohub-1/refs/heads/main/%s/%s"):format(placeId, scriptName)
+    pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+end
