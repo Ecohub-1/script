a9126b7e118f38889900ba1d8f24df3fd5e4da27
Ecohub-1 <boyyanamalila@gmail.com>
2025-05-05 07:21:11 +0700

    Update Loading.lua

--- Loading.lua
+++ Loading.lua
@@ -6,7 +6,7 @@ local placeId = game.PlaceId
 local scriptName = placeScripts[placeId]
 
 if scriptName then
-    local url = ("https://raw.githubusercontent.com/Ecohub-1/Ecohub-1/refs/heads/main/%s/%s"):format(placeId, scriptName)
+    local url = ("https://raw.githubusercontent.com/Ecohub-1/Ecohub-1/refs/heads/main/%s/%s"):format(scriptName)
     pcall(function()
         loadstring(game:HttpGet(url))()
     end)
